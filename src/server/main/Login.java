8cd46f97f73bb1d98bd0c49a411ec1365a4b120c
Matt Carlile <mc940@uowmail.edu.au>
2024-05-01 09:47:26 +1000

    Wrote Login Class with Tests

---
+++ src/server/main/Login.java
@@ -0,0 +1,51 @@
+public class Login {
+	private int LoginID;
+	private String username;
+	private String password;
+	private static int number = 0;
+
+	public Login(String inUser, String inPass) {
+		this.LoginID = number;
+		this.username = inUser;
+		this.password = inPass;
+		number++;
+	}
+
+	public Login(int inLog, String inUser, String inPass) {
+		this.LoginID = inLog;
+		this.username = inUser;
+		this.password = inPass;
+	}
+
+	public static void setNumber(int num) {
+		number = num;
+	}
+
+	public static int getNumber() {
+		return number-1;
+	}
+
+	public int getLoginID() {
+		return LoginID;
+	}
+
+	public void setLoginID(int inLog) {
+		LoginID = inLog;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setUsername(String inUser) {
+		username = inUser;
+	}
+
+	public void setPassword(String inPass) {
+		password = inPass;
+	}
+}
