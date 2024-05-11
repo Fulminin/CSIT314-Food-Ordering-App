0846d9f6cadbde86d5f5654da2b48596b74d70d1
20977 <terence.yuanz@gmail.com>
2024-05-11 18:36:51 +1000

    change src files

---
+++ DingGoVite/src/server/test/TestTesting.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTesting {
+	Testing test = new Testing();
+
+	@Test
+	public void add1() {
+		assertSame(2, test.add1(1));
+	}
+
+	@Test 
+	public void add2() {
+		assertSame(3, test.add2(1));
+	}
+}
