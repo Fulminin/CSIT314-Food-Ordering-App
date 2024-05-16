b8b303bf47c85eded5200ba3dfdb43ea88c3d4cc
rm514 <162117550+rm514@users.noreply.github.com>
2024-05-16 21:10:33 +1000

    Navbar

--- DingGoVite/src/Components/Navbar/Navbar/Navbar.jsx
+++ DingGoVite/src/Components/Navbar/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../../assets/assets'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <img src={assets.DingGoIcon} alt="DingGo Icon" className='logoBell' />
+      <img src={assets.DingGoName} alt="DingGo Name" className='logoName' />
+
+      <div classname='navbarRight'>
+        <button>Sign up</button>
+        <button>Sign in</button>
+      </div>
+
     </div>
   )
 }
