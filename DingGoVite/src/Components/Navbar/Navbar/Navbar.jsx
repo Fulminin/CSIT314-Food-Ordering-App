74566914d5c6bbf400c0289723eb68c2e0acf654
rm514 <162117550+rm514@users.noreply.github.com>
2024-05-16 20:03:04 +1000

    Additions

--- DingGoVite/src/Components/Navbar/Navbar/Navbar.jsx
+++ DingGoVite/src/Components/Navbar/Navbar/Navbar.jsx
@@ -3,9 +3,8 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className='Navbar'>
-        <img src='./Assets/DingGoIcon.png' alt='Dingo Icon'/>
-        <img src='./Assets/DingGoName.png' alt='Dingo Name'/>
+    <div className='navbar'>
+      
     </div>
   )
 }
---
+++ DingGoVite/src/assets/assets.js
@@ -0,0 +1,116 @@
+import aquamarineGrille from './aquamarineGrille.png';
+import beefUdon from './beefUdon.png';
+import bento from './bento.png';
+import bentoHouse from './bentoHouse.png';
+import bentoHouseBanner from './bentoHouseBanner.png';
+import bentoIcon from './bentoIcon.png';
+import bomboraSeafood from './bomboraSeafood.png';
+import bullAndBear from './bullAndBear.png';
+import chickenKatsuDon from './chickenKatsuDon.png';
+import chickenKBento from './chickenKBento.png';
+import chickenTeriyakiBento from './chickenTeriyakiBento.png';
+import cnFood from './cnFood.png';
+import deliveryDriver from './deliveryDriver.png';
+import diggies from './diggies.png';
+import DingGo_logo from './DingGo_logo.png';
+import DingGoIcon from './DingGoIcon.png';
+import DingGoName from './DingGoName.png';
+import donburiIcon from './donburiIcon.png';
+import drinksIcon from './drinksIcon.png';
+import editInfo from './editInfo.png';
+import frFood from './frFood.png';
+import gerFood from './gerFood.png';
+import googlepay from './googlepay.png';
+import hisBoyElroy from './hisBoyElroy.png';
+import homePageBanner from './homePageBanner.png';
+import inFood from './inFood.png';
+import italianIcon from './italianIcon.png';
+import japIcon from './japIcon.png';
+import krFood from './krFood.png';
+import mastercard from './mastercard.png';
+import menu from './menu.png';
+import mxFood from './mxFood.png';
+import omuRice from './omuRice.png';
+import omuriceIcon from './omuriceIcon.png';
+import orderCancled from './orderCancled.png';
+import palisade from './palisade.png';
+import paneraSoup from './paneraSoup.png';
+import paypal from './paypal.png';
+import place from './place.png';
+import profile from './profile.png';
+import promoImage from './promoImage.png';
+import ramenIcon from './ramenIcon.png';
+import saladIcon from './saladIcon.png';
+import SalmonTBento from './SalmonTBento.png';
+import shoppingCart from './shopping-cart.png';
+import sidesIcon from './sidesIcon.png';
+import soupsIcon from './soupsIcon.png';
+import spFood from './spFood.png';
+import sSushiPlater from './sSushiPlater.png';
+import sushi from './sushi.png';
+import takoyaki from './takoyaki.png';
+import thFood from './thFood.png';
+import tiles from './tiles.png';
+import tonkotsuRamen from './tonkotsuRamen.png';
+import udonIcon from './udonIcon.png';
+import visapay from './visapay.png';
+
+
+export const assets = {
+    aquamarineGrille,
+    beefUdon,
+    bento,
+    bentoHouse,
+    bentoHouseBanner,
+    bentoIcon,
+    bomboraSeafood,
+    bullAndBear,
+    chickenKatsuDon,
+    chickenKBento,
+    chickenTeriyakiBento,
+    cnFood,
+    deliveryDriver,
+    diggies,
+    DingGo_logo,
+    DingGoIcon,
+    DingGoName,
+    donburiIcon,
+    drinksIcon,
+    editInfo,
+    frFood,
+    gerFood,
+    googlepay,
+    hisBoyElroy,
+    homePageBanner,
+    inFood,
+    italianIcon,
+    japIcon,
+    krFood,
+    mastercard,
+    menu,
+    mxFood,
+    omuRice,
+    omuriceIcon,
+    orderCancled,
+    palisade,
+    paneraSoup,
+    paypal,
+    place,
+    profile,
+    promoImage,
+    ramenIcon,
+    saladIcon,
+    SalmonTBento,
+    shoppingCart,
+    sidesIcon,
+    soupsIcon,
+    spFood,
+    sSushiPlater,
+    sushi,
+    takoyaki,
+    thFood,
+    tiles,
+    tonkotsuRamen,
+    udonIcon,
+    visapay
+}
