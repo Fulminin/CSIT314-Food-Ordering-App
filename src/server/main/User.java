a8e993c81d149f667a6a709d9643c11be4ba6cf7
Matt Carlile <mc940@uowmail.edu.au>
2024-05-20 23:01:25 +1000

    User class update

--- src/server/main/User.java
+++ src/server/main/User.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class User {
-	public int userId;
-	public int loginId;
-	public String firstName;
-	public String lastName;
-	public double credit;
-	public MemberStatus memberStatus;
+	private int userId;
+	private int loginId;
+	private String firstName;
+	private String lastName;
+	private double credit;
+	private MemberStatus memberStatus;
+	private ArrayList<Address> address;
 
 	public User(int userId, int loginId, String firstName, String lastName, double credit, MemberStatus memberStatus) {
 		this.userId = userId;
@@ -13,5 +17,90 @@ public class User {
 		this.lastName = lastName;
 		this.credit = credit;
 		this.memberStatus = memberStatus;
+		this.address = new ArrayList<Address>();
+	}
+
+	// userId
+	public int getId() {
+		return userId;
+	}
+
+	public boolean setId(int in) {
+		this.userId = in;
+		return true;
+	}
+
+	// loginId
+	public int getloginId() {
+		return loginId;
+	}
+
+	public boolean setLoginId(int id) {
+		this.loginId = id;
+		return true;
+	}
+
+	// firstname
+	public String getfirstname() {
+		return firstName;
+	}
+
+	public boolean setFirstName(String fname) {
+		this.firstName = fname;
+		return true;
+	}
+
+	// lastname
+	public String getLastname() {
+		return lastName;
+	}
+
+	public boolean setLastName(String lname) {
+		this.lastName = lname;
+		return true;
+	}
+
+	// credit
+	public double getCredit() {
+		return credit;
+	}
+
+	public boolean setCredit(double cred) {
+		this.credit = cred;
+		return true;
+	}
+
+	// memberStatus
+	public MemberStatus getMemberStatus() {
+		return memberStatus;
+	}
+
+	public boolean setMemberStatus(MemberStatus input) {
+		this.memberStatus = input;
+		return true;
+	}
+
+	// address
+	public boolean addAddress(Address input){
+		this.address.add(input);
+		return true;
+	}
+
+	public boolean addAddressList(Address[] input) {
+		this.address.addAll(Arrays.asList(input));
+		return true;
+	}
+
+	public boolean removeAddress(int i) {
+		this.address.remove(i);
+		return true;
+	}
+
+	public Address getAddress(int i) {
+		return address.get(i);
+	}
+
+	public ArrayList<Address> getAddressList() {
+		return address;
 	}
 }
