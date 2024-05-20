3697910f759e54762541288014da81a7190dbad2
Matt Carlile <mc940@uowmail.edu.au>
2024-05-21 00:16:05 +1000

    Address Class Update

--- src/server/main/Address.java
+++ src/server/main/Address.java
@@ -1,3 +1,86 @@
 public class Address {
+	private int addressId;
+	private int postcode;
+	private String city;
+	private String streetName;
+	private int streetNum;
+	private int unitNum;
 
+	public Address(int addressId, int postcode, String city, String streetName, int streetNum, int unitNum){
+		this.addressId = addressId;
+		this.postcode = postcode;
+		this.city = city;
+		this.streetName = streetName;
+		this.streetNum = streetNum;
+		this.unitNum = unitNum;
+	}
+
+	public Address(int addressId, int postcode, String city, String streetName, int streetNum) {
+		this.addressId = addressId;
+		this.postcode = postcode;
+		this.city = city;
+		this.streetName = streetName;
+		this.streetNum = streetNum;
+		this.unitNum = 0;
+	}
+
+	// addressid
+	public int getId() {
+		return addressId;
+	}
+
+	public boolean setId(int input) {
+		this.addressId = input;
+		return true;
+	}
+
+	// postcode
+	public int getPostcode() {
+		return postcode;
+	}
+
+	public boolean setPostcode(int input) {
+		this.postcode = input;
+		return true;
+	}
+
+	// city
+	public String getCity() {
+		return city;
+	}
+
+	public boolean setCity(String input) {
+		this.city = input;
+		return true;
+	}
+
+	// streetName
+	public String getStreet() {
+		return streetName;
+	}
+
+	public boolean setStreet(String input) {
+		this.streetName = input;
+		return true;
+	}
+
+	// streetNum
+	public int getStreetNum() {
+		return streetNum;
+	}
+
+	public boolean setStreetNum(int input) {
+		this.streetNum = input;
+		return true;
+	}
+
+	// unitNum
+	public int getUnitNum() {
+		return unitNum;
+	}
+
+	public boolean setUnitNum(int input) {
+		this.unitNum = input;
+		return true;
+	}
 }
