43ca27b864b1fbb5532c5a0480e95a6639ac8b2c
20977 <terence.yuanz@gmail.com>
2024-05-23 08:41:58 +1000

    report

--- src/pages/report/report.jsx
+++ src/pages/report/report.jsx
@@ -5,7 +5,6 @@ import { assets } from '../../Assets/assets';
 import StarRating from '../../Components/StarRating/StarRating';
 
 const Report = () => {
-
   // Function to generate random height for each bar
   const generateRandomHeight = () => {
     return Math.floor(Math.random() * 100) + 5; // Random height between 1 and 100
@@ -17,23 +16,12 @@ const Report = () => {
   const labels = ['Jan', 'Feb', 'Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
   const labels_2 = ['Mon', 'Tue', 'Wed','Thu','Fri','Sat','Sun'];
 
-
   return (
     <div className='report'>
       <div className='banner-container'>
         <Link to="/homeRO" className='back'>Back to home page</Link>
         <img src={assets.bentoHouseBanner} alt="banner" className='banner' />
       </div>
-
-      <div className='report_content'>
-      <p className='rating'>Average Ratings <StarRating /></p>
-        <div>
-            <p className='rpt_title'>
-                <img src={assets.tiles} alt="rpt" className='rpt' />
-                Revenue Report
-            </p>
-            <hr/>
-
       <div className='report_content '>
         <p className='rating'>Average Ratings <StarRating /></p>
         <div>
